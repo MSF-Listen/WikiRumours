6b6d831f8f691701a660edf76de5381c9a10801f
Daniel Fairhead <daniel@dev.ngo>
2022-09-23 17:00:02 +0100

    Absolute defined paths for uploads / downloads

--- source/config/autoload/file_paths.php
+++ source/config/autoload/file_paths.php
@@ -3,4 +3,7 @@
 	$pathToImageMagick = ''; // e.g. /usr/bin/
 	$pathToFFmpeg = ''; // e.g. /usr/local/dh/bin/
 
+	define('SITE_ROOT_DIR', getenv('SITE_ROOT_DIR') ?: '/srv/');
+	define('UPLOADS_ROOT', getenv('UPLOADS_ROOT') ?: (SITE_ROOT_DIR . 'uploads/'));
+
 ?>
